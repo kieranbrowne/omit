a3fb1e36744f9d86dba1af27f39f949d9a54e624
Kieran Browne <kierantbrowne@gmail.com>
2015-08-22 14:28:01 +1000

    introduce content between {}  and array_map for equally nested elements

--- omit.php
+++ omit.php
@@ -7,7 +7,9 @@ function omit($string, $content = []) {
 
 function oParse($tags) {
   $tag = array_shift($tags); // pop tag at top of nest
-    return implode('',array_map(function($t) use ($tags) {return startTag($t) . ((sizeof($tags)>0)?oParse($tags):'') . endTag($t);},parsePlus(parseAsterisk($tag))));
+  return implode('',array_map(function($t) use ($tags) {
+    return startTag($t) . parseContent($t) . ((sizeof($tags)>0)?oParse($tags):'') . endTag($t);
+  },parsePlus(parseAsterisk($tag))));
 }
 
 function startTag($str) { return '<' . parseId(parseClass($str)) . '>'; }
@@ -19,12 +21,16 @@ function oMult($s) { return ((strpos($s,'*')!==false) ?
   intval(preg_replace('/[^0-9+]/','',$s)) : 1); }
 function parsePlus($s) { return explode('+',$s); }
 function parseAsterisk($s) { 
-  return implode('+', array_fill(0,oMult($s),preg_replace('/[0-9+\*]+/', '', $s)));}
-function parseContent($s,$content) {
-  return implode('+', array_map(function($c) use ($s) { return preg_replace('/[\$]/','',$s) .'{'.$c.'}'; },$content));
-}
+  return implode('+', array_fill(0,oMult($s),preg_replace('/[0-9*]+/', '', $s)));}
+function oContent($s,$content) {
+  return implode('+', array_map(function($c) use ($s) { return preg_replace('/[\$]/','',$s) .'{'.$c.'}'; },$content));}
+function parseContent($s) { preg_match('~{(.*?)}~',$s, $out);
+  return ((sizeof($out)>0) ? $out[1] : ''); }
 
-omit('div#wrapper>span.title>ul>li*3');
 
+omit('div#wrapper>span.title{Title}+ul>li*3');
 
+assert(parsePlus('span.class{this}+ul') === ['span.class{this}','ul'], "parsePlus");
+assert(parseContent('span.class{this is the content}') === 'this is the content', "parseContent");
+assert(parsePlus(parseAsterisk('span.title{Title}+ul')) === ['span.title{Title}','ul']);
 ?>
