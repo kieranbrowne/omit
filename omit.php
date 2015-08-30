ff5fbb4907c1ef5e02833f58cc3844466e3485de
Kieran Browne <kierantbrowne@gmail.com>
2015-08-31 01:19:52 +1000

    attributes working

--- omit.php
+++ omit.php
@@ -1,18 +1,18 @@
 <?php
 
 function omit($string, $content = []) {
-  echo oParse(explode('>',$string),$content);
+  echo parseNest($string,$content);
 }
 
 function oParse($tags,$content=[]) {
   $parts = explode('+',(oFunc(oContent(parseAsterisk(array_shift($tags)),$content),$content)));
   $last = array_pop($parts);
   return array_reduce($parts,
-    function ($carry, $item) use ($tags) { return $carry . toHtml($item); } ) . startTag($last) . oGet($last) . ((sizeof($tags)>0)?oParse($tags,$content):'') .endTag($last); 
+    function ($carry, $item) use ($tags) { return $carry . toHtml($item); } ) . startTag($last) .  ((sizeof($tags)>0)?oParse($tags,$content):'') .endTag($last); 
 }
 
-function toHtml($tag) { return startTag($tag) . oGet($tag) . endTag($tag); }
-function startTag($str) { return '<' . oTag($str)['name'] . (ohas($str,'#')?' id="'.oTag($str)['id'].'"':'') .(ohas($str,'.')?' class="'.oTag($str)['class'].'"':'') . '>'; }
+function toHtml($tag) { return startTag($tag) . endTag($tag); }
+function startTag($str) { return '<' . oTag($str)['name'] . (ohas($str,'#')?' id="'.oTag($str)['id'].'"':'') .(ohas($str,'.')?' class="'.oTag($str)['class'].'"':'') .(ohas($str,'[')?' '.oTag($str)['attr']:''). '>'.oGet($str); }
 function endTag($str) { return '</' . oTag($str)['name'] . '>'; }
 function oMult($s) { return ((strpos($s,'*')!==false) ? intval(preg_replace('/[^0-9+]/','',$s)) : 1); }
 //function parsePlus($s) { return explode('+',$s); }
@@ -21,11 +21,12 @@ function oContent($s,$content) { return ((strpos($s,'$')!==false)?implode('+', a
 function oGet($str,$start='{',$end='}') {
   return ((strpos($str,str_replace('\\','',$start))!==false)?preg_split('/'.$end.'/',preg_split('/'.$start.'/',$str)[1])[0]:'');}
 function oTag($t) { 
+  preg_match_all("/\[([^\]]*)\]/", $t, $attrs);
   return array( 
   'name' => preg_split('/[^[:alnum:]]+/', $t)[0], 
   'id' => oGet($t,'#','\W'), 
   'class' => oGet($t,'\.','\W'), 
-  'attr' => '', 
+  'attr' => implode(' ',array_map(function($x){return pre($x,'=').'="'.post($x,'=').'"';},$attrs[1])), 
   'content' => '', 
   ); 
 }
@@ -58,11 +59,6 @@ function firstOf($list,$str) {
 
 function pre($str,$char){return substr($str,0,strpos($str,$char));}
 function post($str,$char){return substr($str,strpos($str,$char)+1);}
-//function oMatch($str,$char,$match,$offset=0) {
-  //if(firstOf([$char,$match],post($str,$char)) === $char)
-    //return oMatch(substr_replace($str,'',occurences('(',$str)[1],oMatch(),$char,$match) + strpos($str,$match)+1;
-  //else return strpos($str,$match);
-//}
 function flip($char){ return ['('=>')',')'=>'(','['=>']','{'=>'}'][$char];}
 function match($str,$char) {
   $depth = 0;
@@ -77,18 +73,18 @@ function match($str,$char) {
 function inParen($str) {
   return substr($str,strpos($str,'(')+1,match($str,'(')-strpos($str,'(')-1); }
   
-function parseNest($str) {
+function parseNest($str,$content=[]) {
   switch (firstOf(['(','>','+'],$str)) {
   case '(': 
     return startTag(pre($str,'(')).parseNest(inParen($str)).parseNest(substr($str,match($str,'(')+1)).endTag(pre($str,'(')); break;
-    case '>': return startTag(pre($str,'>')).parseNest(post($str,'>')).endTag(pre($str,'>')); break;
-    case '+': return startTag(pre($str,firstOf(['(','>','+'],substr($str,1)))).endTag(pre($str,firstOf(['(','>','+'],substr($str,1)))).parseNest(post($str,firstOf(['(','>','+'],substr($str,1)))); break;
-    default: return startTag($str).endTag($str); break;
+  case '>': return startTag(pre($str,'>')).parseNest(post($str,'>')).endTag(pre($str,'>')); break;
+  case '+': return startTag(pre($str,firstOf(['(','>','+'],substr($str,1)))).endTag(pre($str,firstOf(['(','>','+'],substr($str,1)))).parseNest(post($str,firstOf(['(','>','+'],substr($str,1)))); break;
+  default: return startTag($str).endTag($str); break;
   }
 }
 echo '<br><br>';
 //echo $test[match($test,'(')].' at '.match($test,'(');
 
-print_r(parseNest('div>span(this>that)span+spin>this'));
-echo 'test of inParen: '.inParen('this(that()this)that()');
+print_r(parseNest('div>span[type=blank][href=dis](div.class{winn}>div#what?)span+spin>this{that}'));
+var_dump(oTag('this[src=blah][sauce=blar]')['attr']);
 ?>
