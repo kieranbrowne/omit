026b9a58bcee40f70f037b55e86fe49485461b14
Kieran Browne <kierantbrowne@gmail.com>
2015-08-31 01:25:13 +1000

    removed obsolete functions

--- omit.php
+++ omit.php
@@ -4,18 +4,9 @@ function omit($string, $content = []) {
   echo parseNest($string,$content);
 }
 
-function oParse($tags,$content=[]) {
-  $parts = explode('+',(oFunc(oContent(parseAsterisk(array_shift($tags)),$content),$content)));
-  $last = array_pop($parts);
-  return array_reduce($parts,
-    function ($carry, $item) use ($tags) { return $carry . toHtml($item); } ) . startTag($last) .  ((sizeof($tags)>0)?oParse($tags,$content):'') .endTag($last); 
-}
-
-function toHtml($tag) { return startTag($tag) . endTag($tag); }
 function startTag($str) { return '<' . oTag($str)['name'] . (ohas($str,'#')?' id="'.oTag($str)['id'].'"':'') .(ohas($str,'.')?' class="'.oTag($str)['class'].'"':'') .(ohas($str,'[')?' '.oTag($str)['attr']:''). '>'.oGet($str); }
 function endTag($str) { return '</' . oTag($str)['name'] . '>'; }
 function oMult($s) { return ((strpos($s,'*')!==false) ? intval(preg_replace('/[^0-9+]/','',$s)) : 1); }
-//function parsePlus($s) { return explode('+',$s); }
 function parseAsterisk($s) { return implode('+', array_fill(0,oMult($s),preg_replace('/^[*]+/', '', $s)));}
 function oContent($s,$content) { return ((strpos($s,'$')!==false)?implode('+', array_map(function($c) use ($s) { return preg_replace('/[\$]/','',$s) .'{'.$c.'}'; },$content)):$s);}
 function oGet($str,$start='{',$end='}') {
@@ -32,8 +23,8 @@ function oTag($t) {
 }
 function oHas($tag,$str) { return ((strpos($tag,$str)!==false)?true:false); }
 function oFunc($t,$content) { return ((strpos($t,'|')!==false)?implode('+',array_map(function($c) use ($t) { return preg_replace('/\|([^\|]+)\|/','',$t).'{'.$c.'}';},array_map(oGet($t,'\|','\|'),$content))):$t);}
-function strInside($str,$start,$end){
-  return ((strpos($str,str_replace('\\','',$start))!==false)?preg_split('/$end/',preg_split('/'.$start.'/',$str)[1])[0]:'fail');}
+//function strInside($str,$start,$end){
+ // return ((strpos($str,str_replace('\\','',$start))!==false)?preg_split('/$end/',preg_split('/'.$start.'/',$str)[1])[0]:'fail');}
 
 function parseParentheses($str) {
   if(strpos($str,'(') !== false) {
@@ -83,8 +74,6 @@ function parseNest($str,$content=[]) {
   }
 }
 echo '<br><br>';
-//echo $test[match($test,'(')].' at '.match($test,'(');
 
 print_r(parseNest('div>span[type=blank][href=dis](div.class{winn}>div#what?)span+spin>this{that}'));
-var_dump(oTag('this[src=blah][sauce=blar]')['attr']);
 ?>
